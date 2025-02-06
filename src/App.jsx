441c8a81e8b72ed461fcb340e4d4ef8d8c22d71c
Hans Bregman <hns101@live.nl>
2025-02-06 17:15:07 +0100

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import ButtonCompoment from "./compoments/button/ButtonCompoment.jsx";
 
 function App() {
-  return (
+  return (<>
       <h1>Handbags & Purses</h1>
-  )
+      <nav>
+        <ButtonCompoment title="to the collection" disableButton={false}  />
+        <ButtonCompoment title="shop all bags" disableButton={false}  />
+        <ButtonCompoment title="pre-orders" disableButton={true}  />
+      </nav>
+    <main>
+
+    </main>
+  </>)
 }
 
 export default App
